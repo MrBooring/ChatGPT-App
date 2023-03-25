0a712d1b8ad5b19d422bd5ec523167567efeb402
MrBooring <pyiush.vedpathak@gmail.com>
2023-03-25 19:53:56 +0530

    blank key

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,7 +5,7 @@ Color scaffoldBackgroundColor = const Color(0xFF343541);
 Color cardColor = const Color(0xFF444654);
 Size size = Get.size;
 String baseuri = "https://api.openai.com";
-String key = "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";
+String key = <<------your api key here------->> ;
 String apiid = "gpt-3.5-turbo";
-String apiroot = "text-davinci-003";
-int apicreated = 1674508855;
+ 
+ 
